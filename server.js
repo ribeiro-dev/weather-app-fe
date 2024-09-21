e0b677bc4cdd70b864b4e0869571aae2fe4827b4
ribeiro-dev <romilson.2r@gmail.com>
2024-09-21 11:14:33 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const path = require("path")
+
+const app = express()
+const port = 3000
+
+app.use(express.static(path.join(__dirname, 'src', 'public')))
+
+app.get('/', (req, res) => {
+   res.sendFile('index.html');
+})
+
+
+
+app.listen(port, () => {
+   console.log(`Server listening on port ${port}`)
+})
