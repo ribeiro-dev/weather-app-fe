03c031dfef2a220ba9f85bc91da13cf3655b70fa
ribeiro-dev <romilson.2r@gmail.com>
2024-09-21 11:29:48 -0300

    move public folder to root directory

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path")
 const app = express()
 const port = 3000
 
-app.use(express.static(path.join(__dirname, 'src', 'public')))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
    res.sendFile('index.html');
